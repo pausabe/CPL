84f6403b177fb96bd30740e7858a4796266a69f4
Alex <rgalex2034@hotmail.com>
2017-04-23 02:25:31 +0200

    Added setter methods for the settings. Also, added error on the Promises.

--- src/Settings/SettingsManager.js
+++ src/Settings/SettingsManager.js
@@ -9,11 +9,11 @@ const diocesis = {
 
 
 const defaultSettings = {
-    showGlories: false,
-    useLatin: false,
-    textSize: 18,
+    showGlories: "false",
+    useLatin: "false",
+    textSize: "18",
     diocesis: diocesis.BARCELONA,
-    dayStart: 0 //Values from 0 to 3 allowed, which means 00:00AM, 01:00AM, 02:00AM and 03:00AM
+    dayStart: "0" //Values from 0 to 3 allowed, which means 00:00AM, 01:00AM, 02:00AM and 03:00AM
 };
 
 export default class SettingsManager{
@@ -24,9 +24,13 @@ export default class SettingsManager{
     * Returns an asynchronous Promise with the callback set when callback is a Function, if not, returns just the Promise.
     */
     static _getStorageValue(key, callback, defaultValue){
-        let storagePromise = AsyncStorage.getItem(key);
+        let getPromise = AsyncStorage.getItem(key);
         let settingsPromise = new Promise((resolve, reject) => {
-            storagePromise.then(value => resolve(value == null ? defaultValue : value));
+            getPromise.then(
+                value => resolve(value == null ? defaultValue : value)
+            ).catch(
+                error => reject(error)
+            );
         });
         if(callback instanceof Function){
             settingsPromise.then(callback);
@@ -34,12 +38,29 @@ export default class SettingsManager{
         return settingsPromise;
     }
 
+    static _setStorageValue(key, value, callback){
+        let savePromise = AsyncStorage.setItem(key, value);
+        savePromise.then(callback);
+        return savePromise;
+    }
+
+    static _setValueIfValid(key, value, validateFunc, callback){
+        if(!(validateFunc instanceof Function) || validateFunc(value)){
+            return SettingsManager._setStorageValue(key, value, callback);
+        }else{
+            let wrongValuePromise = new Promise((resolve, reject) => {
+                reject(new Error("Invalid value"));
+            });
+            return wrongValuePromise;
+        }
+    }
+
     static getSettingShowGlories(callback){
         return SettingsManager._getStorageValue("showGlories", callback, defaultSettings.showGlories);
     }
 
     static getSettingUseLatin(callback){
-        return SettingsManager._getStorageValue("showLatin", callback, defaultSettings.useLatin);
+        return SettingsManager._getStorageValue("useLatin", callback, defaultSettings.useLatin);
     }
 
     static getSettingTextSize(callback){
@@ -52,6 +73,43 @@ export default class SettingsManager{
 
     static getSettingDayStart(callback){
         return SettingsManager._getStorageValue("dayStart", callback, defaultSettings.dayStart);
+    }
+
+    static setSettingShowGlories(value, callback){
+        return SettingsManager._setValueIfValid("showGlories", value,
+            (val) => val === "true" || val === "false",
+            callback);
+    }
+
+    static setSettingUseLatin(value, callback){
+        return SettingsManager._setValueIfValid("useLatin", value,
+            (val) => val === "true" || val === "false",
+            callback);
+    }
+
+    static setSettingTextSize(value, callback){
+        return SettingsManager._setValueIfValid("textSize", value,
+            (val) => !isNaN(val) && (parseFloat(val)*10)%10 == 0,
+            callback);
+    }
+
+    static setSettingDiocesis(value, callback){
+        return SettingsManager._setValueIfValid("diocesis", value,
+            (val) => {
+                let found = false;
+                for(key in diocesis){
+                    if(diocesis[key] === val){
+                        found = true;
+                    }
+                }
+                return found;
+            }, callback);
+    }
+
+    static setSettingDayStart(value, callback){
+        return SettingsManager._setValueIfValid("dayStart", value,
+            (val) => val == "0" || val == "1" || val == "2" || val == "3",
+            callback);
     }
 
 }
