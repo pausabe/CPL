cd978372d05596a038f08360dee902a1e2cd75c0
Pau <pau.sabe@est.fib.upc.edu>
2017-04-24 22:21:40 +0100

    Esquelet of Laudes

--- src/Screens/LiturgiaDisplayScreen.js
+++ src/Screens/LiturgiaDisplayScreen.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, ScrollView, Text, StyleSheet, Platform } from 'react-native';
 
 import Ofici from '../Components/Ofici'
+import Laudes from '../Components/Laudes'
 
 function paddingBar(){
   if(Platform.OS === 'ios'){
@@ -16,12 +17,37 @@ export default class LiturgiaDisplayScreen extends Component {
       barPad = 0;
     }
   }
+
   render() {
     return (
       <View style={styles.container}>
         <ScrollView >
-          {this.props.type === 'Ofici' ?
-            <Ofici
+          {this.liturgicComponent(this.props.type)}
+        </ScrollView>
+      </View>
+    )
+  }
+
+  liturgicComponent(type){
+    switch (type) {
+      case 'Ofici':
+        return(
+          <Ofici
+            hour = {this.props.hour}
+            weekDay = {this.props.weekDay}
+            monthDay = {this.props.monthDay}
+            month = {this.props.month}
+            year = {this.props.year}
+            cicle = {this.props.cicle}
+            ordinariWeek = {this.props.ordinariWeek}
+            pasquaWeek = {this.props.pasquaWeek}
+            quaresmaWeek = {this.props.quaresmaWeek}
+            LT={this.props.LT}/>
+          )
+        break;
+        case 'Laudes':
+          return(
+            <Laudes
               hour = {this.props.hour}
               weekDay = {this.props.weekDay}
               monthDay = {this.props.monthDay}
@@ -32,12 +58,55 @@ export default class LiturgiaDisplayScreen extends Component {
               pasquaWeek = {this.props.pasquaWeek}
               quaresmaWeek = {this.props.quaresmaWeek}
               LT={this.props.LT}/>
-            :
-            <Text style={styles.normalText}>{this.props.type}</Text>
-          }
-        </ScrollView>
-      </View>
-    )
+            )
+          break;
+          case 'Vespres':
+            return(
+              <Laudes
+                hour = {this.props.hour}
+                weekDay = {this.props.weekDay}
+                monthDay = {this.props.monthDay}
+                month = {this.props.month}
+                year = {this.props.year}
+                cicle = {this.props.cicle}
+                ordinariWeek = {this.props.ordinariWeek}
+                pasquaWeek = {this.props.pasquaWeek}
+                quaresmaWeek = {this.props.quaresmaWeek}
+                LT={this.props.LT}/>
+              )
+            break;
+            case 'Hora menor':
+              return(
+                <Laudes
+                  hour = {this.props.hour}
+                  weekDay = {this.props.weekDay}
+                  monthDay = {this.props.monthDay}
+                  month = {this.props.month}
+                  year = {this.props.year}
+                  cicle = {this.props.cicle}
+                  ordinariWeek = {this.props.ordinariWeek}
+                  pasquaWeek = {this.props.pasquaWeek}
+                  quaresmaWeek = {this.props.quaresmaWeek}
+                  LT={this.props.LT}/>
+                )
+              break;
+              case 'Completes':
+                return(
+                  <Laudes
+                    hour = {this.props.hour}
+                    weekDay = {this.props.weekDay}
+                    monthDay = {this.props.monthDay}
+                    month = {this.props.month}
+                    year = {this.props.year}
+                    cicle = {this.props.cicle}
+                    ordinariWeek = {this.props.ordinariWeek}
+                    pasquaWeek = {this.props.pasquaWeek}
+                    quaresmaWeek = {this.props.quaresmaWeek}
+                    LT={this.props.LT}/>
+                  )
+                break;
+      default: return(<Text style={styles.normalText}>{this.props.type}</Text>)
+    }
   }
 }
 
