cb1da8ebf5849aa3252d6c406d873be6efe70040
Pau <pau.sabe@est.fib.upc.edu>
2018-01-08 10:54:17 +0100

    0.5.1 update FINISHED

--- android/app/src/main/java/cpl/cpl/MainActivity.java
+++ android/app/src/main/java/cpl/cpl/MainActivity.java
@@ -1,13 +1,12 @@
 package cpl.cpl;
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
-//import com.horcrux.svg.SvgPackage;
 import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
   @Override
    protected void onCreate(Bundle savedInstanceState) {
-       //SplashScreen.show(this);
+       SplashScreen.show(this);
        super.onCreate(savedInstanceState);
    }
 
