4ce908fcc53e799e257e821d11949cd48175e152
Pau Sabé <pau.sabe@icloud.com>
2019-11-26 21:55:48 +0100

    [v2.2.4] New version

--- src/Globals/Globals.js
+++ src/Globals/Globals.js
@@ -39,7 +39,7 @@ module.exports = {
   size9: 39,
   size10: 42,
 
-  DBName: 'lh_v2.2.2.db',
+  DBName: 'lh_v2.2.4.db',
 
   paddingBar: Platform.OS === 'ios' ? 0 : 54,
 
--- src/Tests/TestsManager.js
+++ src/Tests/TestsManager.js
@@ -8,7 +8,7 @@ export var STATE_ON = TEST_MODE_ON && false;
 export var TEST_FIRST_DAY = new Date(2019, 10, 1); //(s'inclou en el test) Que no sigui "Avoidable"!
 export var TEST_LAST_DAY = new Date(2020, 11, 27); //(s'inclou en el test) Que no sigui "Avoidable"!
 export var FIRST_DIOCESI = 0; //0-33 (s'inclou en el test) Order is determined in GF.getTestDiocesiByIndex()
-export var LAST_DIOCESI = 0; //0-33 (s'inclou en el test)
+export var LAST_DIOCESI = 33; //0-33 (s'inclou en el test)
 
 /************************************************************/
 
